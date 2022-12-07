40ffc53a701f11844c159b58a55691bc6b2562d0
Nate Grabowski <nategrabowski@outlook.com>
2022-12-06 21:48:22 -0700

    Add project files.

---
+++ First Non-Repeating Character/FirstNonRepeatingCharacter/FirstNonRepeatingCharacter/Program.cs
@@ -0,0 +1,63 @@
+﻿//brute force: O(N ^ 2)time
+
+//bool duplicate
+
+//for int i over string length
+
+//nested for loop int j over string length, checking against each char, except for same index
+
+//if there is a match set duplicate to true, if not set to false and return index of char
+
+using System.Collections.Generic;
+string algo = "abcdcaf";
+
+Console.WriteLine(FirstNonRepeatingCharacter(algo));
+
+#region Iteration 1
+
+//int FirstNonRepeatingCharacter(string str)
+//{
+//    for (int i = 0; i < str.Length; i++)
+//    {
+//        bool duplicate = false;
+
+//        for (int j = 0; j < str.Length; j++)
+//        {
+//            if (i == j) { continue; }
+
+//            if (str[i] == str[j])
+//            {
+//                duplicate = true;
+//            }
+//        }
+//        if (!duplicate) { return i; }
+//    }
+//    return -1;
+//}
+
+#endregion
+
+int FirstNonRepeatingCharacter(string str)
+{
+    Dictionary<char, int> dict = new Dictionary<char, int>();
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (!dict.ContainsKey(str[i]))
+        {
+            dict.Add(str[i], 1);
+        }
+        else if (dict.ContainsKey(str[i]))
+        {
+            dict[str[i]]++;
+        }
+    }
+    //if value = 1 return answer
+    foreach (var item in dict)
+    {
+        if (item.Value == 1)
+        {
+            return str.IndexOf(item.Key);
+        }
+    }
+    return -1;
+}
---
+++ Two Number Sum/Two Number Sum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+int[] ints = { 3, 5, -4, 8, 11, 1, -1, 6 };
+int targetSum = 10;
+
+
+static int[] TwoNumberSum(int[] array, int targetSum)
+{
+
+
+    return new int[0];
+}
