a564a39ca304e64c4a5e2b1b7b542bf6154ed769
Nate Grabowski <nategrabowski@outlook.com>
2022-12-07 14:21:51 -0700

    Finished Tournament Winner

--- TournamentWinner/TournamentWinner/Program.cs
+++ TournamentWinner/TournamentWinner/Program.cs
@@ -1,6 +1,12 @@
 ﻿using System.Collections.Generic;
 using System;
+using System.Reflection;
 
+
+/*
+ * O(N) time
+ * O(K) space
+ */
 
 #region Test Data
 var match1 = new List<string>()
@@ -27,39 +33,75 @@ var results = new List<int>()
     0,0,1
 };
 #endregion
-
 Console.WriteLine(TournamentWinner(competitions, results));
+
+
+
+#region Solution 1
+//string TournamentWinner(List<List<string>> competitions, List<int> results)
+//{
+//    var scores = new Dictionary<string, int>();
+
+//    for (int i = 0; i < results.Count; i++)
+//    {
+//        if (results[i] == 0)
+//        {
+//            if (scores.ContainsKey(competitions[i][1]))
+//            {
+//                scores[competitions[i][1]] += 3;
+//            }
+//            else
+//            {
+//                scores.Add(competitions[i][1], 3);
+//            }
+//        }
+
+//        else if (results[i] == 1)
+//        {
+//            if (scores.ContainsKey(competitions[i][0]))
+//            {
+//                scores[competitions[i][0]] += 3;
+//            }
+//            else
+//            {
+//                scores.Add(competitions[i][0], 3);
+//            }
+//        }
+//    }
+//    var maxValueKey = scores.OrderByDescending(x => x.Value).First().Key;
+
+//    return maxValueKey;
+//}
+
+#endregion
+
+
+#region Solution 2
 
 string TournamentWinner(List<List<string>> competitions, List<int> results)
 {
-    var match = new Dictionary<string, int>();
-
-    for (int i = 0; i < results.Count; i++)
+    Dictionary<string, int> score = new Dictionary<string, int>();
+    int topScore = 0;
+    string team = "";
+    for (int i = 0; i < competitions.Count; i++)
     {
-        //var winner = results[i] == 1 ? competitions[i][0] : competitions[i][0];
+        var winner = results[i] == 1 ? competitions[i][0] : competitions[i][1];
+        if (score.ContainsKey(winner))
+        {
+            score[winner] += 3;
+        }
+        else
+        {
+            score.Add(winner, 3);
+        }
+        if (score[winner] > topScore)
+        {
+            topScore = score[winner];
+            team = winner;
+        }
 
-        if (results[i] == 0)
-        {
-            match.Add(competitions[i][1], 3);
-        }
-        else if (results[i] == 1)
-        {
-            match.Add(competitions[i][0], 3);
-        }
     }
-
-    return "";
+    return team;
 }
 
-/*
- * When you win you get 3
- * When you lose you gain 0
- * Never a tie
- * 
- * 
- * bestTeam = ""; in loop
- * compare score of team with current best team
- * 
- * O(N) time
- * O(K) space
- */
+#endregion
