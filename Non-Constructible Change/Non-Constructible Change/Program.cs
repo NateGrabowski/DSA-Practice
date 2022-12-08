7b808fb4a982c1c5d996bb5760069259d7ade25e
Nate Grabowski <nategrabowski@outlook.com>
2022-12-07 23:15:08 -0700

    Finished

--- Non-Constructible Change/Non-Constructible Change/Program.cs
+++ Non-Constructible Change/Non-Constructible Change/Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+int[] coins = { 1, 2, 4 };
+Console.WriteLine(NonConstructibleChange(coins));
+
+int NonConstructibleChange(int[] coins)
+{
+    Array.Sort(coins);
+    int change = 0;
+    for (int i = 0; i < coins.Length; i++)
+    {
+
+
+        if (coins[i] > change + 1)
+        {
+            break;
+        }
+
+        if (coins[i] <= change + 1)
+        {
+            change += coins[i];
+        }
+
+
+    }
+    return change + 1;
+}
