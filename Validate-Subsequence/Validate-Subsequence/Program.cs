9b50f6f9519d97c04667da57f76819bf189d346a
Nate Grabowski <nategrabowski@outlook.com>
2022-12-09 23:07:07 -0700

    Validate-Subsequenence +Array +Sequence

--- Validate-Subsequence/Validate-Subsequence/Program.cs
+++ Validate-Subsequence/Validate-Subsequence/Program.cs
@@ -2,6 +2,12 @@
 Console.WriteLine("Hello, World!");
 
 
+/*
+ * [5, 1, 22, 25, 6, -1, 8, 10]
+ * 
+ * [1, 6, -1, 10]
+ */
+
 bool IsValidSubsequence(List<int> array, List<int> sequence)
 {
     // Write your code here.
