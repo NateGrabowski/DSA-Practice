723c6b94f26f50da9dec3baf8fa710f0d07649ce
Nate Grabowski <nategrabowski@outlook.com>
2022-12-10 18:50:10 -0700

    Minimum-Waiting-Time completed

---
+++ Minimum-Waiting-Time/Minimum-Waiting-Time/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+/*
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+
+int[] ints = { 3, 2, 1, 2, 6 };
+// 6, 3, 2, 2, 1
+
+Console.WriteLine(MinimumWaitingTime(ints));
+
+int MinimumWaitingTime(int[] queries)
+{
+
+    Array.Sort(queries);
+    int currentScore = 0;
+    int newScore = 0;
+
+    for (int i = 0; i < queries.Length - 1; i++)
+    {
+        currentScore += queries[i];
+        newScore += currentScore;
+    }
+    return newScore;
+}
