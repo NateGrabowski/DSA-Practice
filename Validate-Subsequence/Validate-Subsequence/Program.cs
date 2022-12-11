711e99c4ff98e2975596970c0c251a02c2e44726
Nate Grabowski <nategrabowski@outlook.com>
2022-12-10 18:10:27 -0700

    Validate-Subsequences Completed

--- Validate-Subsequence/Validate-Subsequence/Program.cs
+++ Validate-Subsequence/Validate-Subsequence/Program.cs
@@ -1,15 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
 
-/*
- * [5, 1, 22, 25, 6, -1, 8, 10]
- * 
- * [1, 6, -1, 10]
- */
+
+var arr1 = new List<int>()
+{
+    5,1,22,25,6,-1,8,10
+};
+
+var arr2 = new List<int>()
+{
+    6,6,-1,10 //3
+};
+
+
+Console.WriteLine(IsValidSubsequence(arr1, arr2));
 
 bool IsValidSubsequence(List<int> array, List<int> sequence)
 {
-    // Write your code here.
+
+    int index = 0;
+    for (int i = 0; i < array.Count; i++)
+    {
+        if (array[i] == sequence[index])
+        {
+            index++;
+        }
+        if (index == sequence.Count())
+        {
+            return true;
+        }
+    }
     return false;
 }
